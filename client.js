0a976cd026e9524a8b0450bfb685d786fa114302
Mike <mike.simnes@gmail.com>
2023-02-24 08:55:38 -0800

    added move commands to test server response, commented out for now

--- client.js
+++ client.js
@@ -4,10 +4,26 @@ const connect = function() {
     host: "localhost",
     port: 50541
   });
+  conn.setEncoding("utf8");
   conn.on("connect", () => {
     console.log("Name: MLS");
+    // setTimeout(() => {
+    //   console.log("Move: right");
+    //   conn.write("Move: right");
+    // }, 1000);
+    // setTimeout(() => {
+    //   console.log("Move: up");
+    //   conn.write("Move: up");
+    // }, 2000);
+    // setTimeout(() => {
+    //   console.log("Move: left");
+    //   conn.write("Move: left");
+    // }, 3000);
+    // setTimeout(() => {
+    //   console.log("Move: down");
+    //   conn.write("Move: down");
+    // }, 4000);
   });
-  conn.setEncoding("utf8");
   conn.on("data", (data) => {
     console.log(data);
   });
