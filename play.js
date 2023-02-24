589526acd6af9386025b20242611e5ed26d9b02e
Mike <mike.simnes@gmail.com>
2023-02-24 09:54:45 -0800

    refactored event listener and moved functions to input module

--- play.js
+++ play.js
@@ -1,19 +1,6 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log("Connecting ...");
 connect();
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  const handleUserInput = function() {
-    stdin.on("data", (key) => {
-      if (key === "\u0003") {
-        process.exit();
-      }
-    });
-  };
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
 setupInput();
